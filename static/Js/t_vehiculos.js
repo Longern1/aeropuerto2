b29cccca2bf1495672c2bb0b085b1aa58f68063f
Maestre <carlosa-maestreg@unilibre.edu.co>
2025-03-04 15:13:43 -0500

    Delete static directory

--- static/Js/t_vehiculos.js
+++
@@ -1,9 +0,0 @@
-function openModal(vehiclePlate, currentState) {
-    document.getElementById('vehicle_plate').value = vehiclePlate;
-    document.getElementById('current_state').value = currentState;
-    document.getElementById('questionnaireModal').style.display = 'block';
-}
-
-function closeModal() {
-    document.getElementById('questionnaireModal').style.display = 'none';
-}
